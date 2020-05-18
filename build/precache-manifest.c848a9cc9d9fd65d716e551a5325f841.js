52f581bd6ab98eb02e5c63e2124a0b9adfe54d73
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2020-05-18 12:33:30 +0530

    frontend added

---
+++ build/precache-manifest.c848a9cc9d9fd65d716e551a5325f841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570fef2ad36d093588bdf1be417d8833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62004c035923336694c9",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "c66c10b68305e0a5aa23",
+    "url": "/static/js/2.1c071c7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c071c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62004c035923336694c9",
+    "url": "/static/js/main.4bc78599.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
